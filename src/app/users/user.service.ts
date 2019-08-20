fc43a6873e2b9a3a3ee794174fe8d21addb8a5e0
Tommy Bonderenka <tbonderenka@rednovalabs.com>
2019-08-20 12:57:29 -0500

    rearrange code

--- src/app/users/user.service.ts
+++ src/app/users/user.service.ts
@@ -10,49 +10,44 @@ export class UserService {
   constructor(private apollo: Apollo) {}
 
   getUsers() {
-    return this.apollo
-      .watchQuery<Response>({
-        query: CurrentUsers,
-        fetchPolicy: "network-only"
-      })
-      .valueChanges
+    return this.apollo.watchQuery<Response>({
+      query: CurrentUsers,
+      fetchPolicy: "network-only"
+    })
+    .valueChanges
   }
 
   deleteUser(userId) {
-    return this.apollo
-      .mutate({
-        mutation: DeleteUser,
-        variables: {
-          id: userId
-        }
-      })
+    return this.apollo.mutate({
+      mutation: DeleteUser,
+      variables: {
+        id: userId
+      }
+    })
   }
 
   createUser(userDetails) {
-    return this.apollo
-        .mutate({
-          mutation: CreateUser,
-          variables: userDetails
-        })
+    return this.apollo.mutate({
+      mutation: CreateUser,
+      variables: userDetails
+    })
   }
 
   getUser(userId) {
-    return this.apollo
-              .watchQuery<Response>({
-                query: GetUser,
-                variables: {
-                  id: userId
-                },
-                fetchPolicy: "network-only"
-              })
-              .valueChanges
+    return this.apollo.watchQuery<Response>({
+      query: GetUser,
+      variables: {
+        id: userId
+      },
+      fetchPolicy: "network-only"
+    })
+    .valueChanges
   }
 
   updateUser(userDetails) {
-    return this.apollo
-        .mutate({
-          mutation: UpdateUser,
-          variables: userDetails
-        })
+    return this.apollo.mutate({
+      mutation: UpdateUser,
+      variables: userDetails
+    })
   }
 }
